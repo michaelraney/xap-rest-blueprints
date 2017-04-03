b18fb6d01210951cebea73563907c38b47832b70
michaelraney <raneymike83@gmail.com>
2017-04-03 14:52:45 -0400

    Refactor

--- jenkins-pipeline-examples/deploy-empty-space/jenkins-pipeline.groovy
+++ jenkins-pipeline-examples/deploy-empty-space/jenkins-pipeline.groovy
@@ -1,26 +1,32 @@
 #!/usr/bin/env groovy
+//Jenkins Pipleline example demonstrating XAP Deployment using REST services
 //Requires Http Request Plugin https://plugins.jenkins.io/http_request
-//Deploys an Empty Space to the Grid and prints space status 
-stage('available host check'){
-  def availableHostResponse =    httpRequest httpMode:"GET", contentType:"APPLICATION_JSON", consoleLogResponseBody:true, url:"http://172.17.0.3:8090/v1/hosts"
-  println('availableHostResponse Response Code'+availableHostResponse.status)
-  println("availableHostResponse Content: "+availableHostResponse.content)
+//Creates new container and Deploys an Empty Space to the Service Grid
+
+//Print Available Host in Service Grid
+stage('Available Hosts Check'){
+  def availableHostResponse =    httpRequest httpMode:"GET", consoleLogResponseBody:true, contentType:"APPLICATION_JSON",  url:"http://172.17.0.3:8090/v1/hosts"
+  println("availableHostResponse Response Code "+availableHostResponse.status)
+  println("availableHostResponse Content: "     +availableHostResponse.content)
 }
 
-stage('create container'){
-  def createContainerResponse =  httpRequest httpMode:"POST", contentType:"APPLICATION_JSON", requestBody:"{ \"host\": \"172.17.0.3\", \"memory\": \"512m\" }", consoleLogResponseBody:true, url:"http://172.17.0.3:8090/v1/containers"
-  println('createContainerResponse Response Code: '+createContainerResponse.status)
-  sleep(10)// Create Container is asynchronous
+//Create a new GSC Container Resource
+stage('Create Container'){
+  def createContainerResponse =  httpRequest httpMode:"POST", consoleLogResponseBody:true, contentType:"APPLICATION_JSON", requestBody:"{ \"host\": \"172.17.0.3\", \"memory\": \"512m\" }", url:"http://172.17.0.3:8090/v1/containers"
+  println("createContainerResponse Response Code: "+createContainerResponse.status)
+  sleep(10)//Create Container is asynchronous
 }
 
-stage('deploy empty grid'){
-  def deployEmptySpaceResponse = httpRequest httpMode:"POST", consoleLogResponseBody:true, url:"http://172.17.0.3:8090/v1/spaces?name=dataGrid&partitions=1&backups=false&requiresIsolation=true"
-  println('deployEmptySpaceResponse Response Code: '+deployEmptySpaceResponse.status)
-  sleep(10)// Deploy Space is asynchronous
+//Deploy an Empty Space (Datagrid) to available Container Resource
+stage('Deploy Empty Space'){
+  def deployEmptySpaceResponse = httpRequest httpMode:"POST", consoleLogResponseBody:true, contentType:"APPLICATION_JSON", url:"http://172.17.0.3:8090/v1/spaces?name=dataGrid&partitions=1&backups=false&requiresIsolation=true"
+  println("deployEmptySpaceResponse Response Code: "+deployEmptySpaceResponse.status)
+  sleep(10)//Deploy Space is asynchronous
 }
 
-stage('check available spaces'){
-  def availableSpacesResponse =  httpRequest consoleLogResponseBody:true, url:"http://172.17.0.3:8090/v1/spaces"
-  println('availableSpacesResponse Response Code: '+availableSpacesResponse.status)
-  println("availableSpacesResponse Content: "+availableSpacesResponse.content)
+//Confirm Deployment of Empty Space 
+stage('Available Spaces Check'){
+  def availableSpacesResponse =  httpRequest httpMode:"GET",  consoleLogResponseBody:true, contentType:"APPLICATION_JSON", url:"http://172.17.0.3:8090/v1/spaces"
+  println("availableSpacesResponse Response Code: "+availableSpacesResponse.status)
+  println("availableSpacesResponse Content: "      +availableSpacesResponse.content)
 }
